42feaa2970d8744a3ae80adc342d3e31e34d7cc7
mayank abhijeet <30956104+mayankabhijeet@users.noreply.github.com>
2019-05-09 18:42:10 +0530

    Fix css class

--- src/components/offline.jsx
+++ src/components/offline.jsx
@@ -4,8 +4,8 @@ import "../App.css";
 const OfflinePage = ({ filteredChartData }) => {
   return (
     <Fragment>
-      <div className="bg-dark container-fluid py-5 text-center text-white">
-        <h1 className="set-pos">App is Offline, Please check internet connectivity</h1>
+      <div className="set-pos bg-dark container-fluid py-5 text-center text-white">
+        <h1>App is Offline, Please check internet connectivity</h1>
       </div>
     </Fragment>
   );
